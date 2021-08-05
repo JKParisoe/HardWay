bf0c1d73684a9b15da82e2568116bf0b7d897123
JKParisoe <parisoemusic@gmail.com>
2021-08-05 16:17:42 -0400

    trying to push to git

--- ex1.py
+++ ex1.py
@@ -5,3 +5,7 @@ print("This is fun.")
 print('Yay! Printing.')
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
+print("I'm tired, let's go have potato salad")
+
+print("You don't know how it feels.")
+
